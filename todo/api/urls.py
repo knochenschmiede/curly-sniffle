2175ff75b35f1bd6e0cfd379d03d56d17e280338
Aaron Meinel <aaronrebmann@gmail.com>
2021-10-01 13:04:39 +0200

    Add a basic todo app with REST API

---
+++ todo/api/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from django.conf.urls import url
+from django.urls import path, include
+from .views import TodoListApiView, TodoDetailApiView
+
+urlpatterns = [
+    path("", TodoListApiView.as_view()),
+    path("<int:todo_id>/", TodoDetailApiView.as_view()),
+]
